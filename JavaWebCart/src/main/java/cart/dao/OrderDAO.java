1df815533bed0ef3db0c3278b09cfb9240d74c79
Lucky626-sys <0911869582@gmail.com>
2025-04-30 13:12:42 +0800

    cart

--- JavaWebCart/src/main/java/cart/dao/OrderDAO.java
+++ JavaWebCart/src/main/java/cart/dao/OrderDAO.java
@@ -6,12 +6,13 @@ import cart.model.entity.Order;
 import cart.model.entity.OrderItem;
 
 public interface OrderDAO {
-	//建立訂單阻擋，回傳訂單編號
-	Integer addOrder(Integer addId);
-	//建立訂單項目(訂單明細檔)
+	// 建立訂單主檔, 回傳訂單編號
+	Integer addOrder(Integer userId);
+	// 建立訂單項目(訂單明細檔)
 	void addOrderItem(Integer orderId, Integer productId, Integer quantity);
-	//查詢使用者訂單
+	
+	// 查詢使用者訂單
 	List<Order> findAllOrdersByUserId(Integer userId);
-	//查詢訂單明細
-	List<OrderItem> findAllOrdersItemByOrderId(Integer orderId);
+	// 查詢訂單明細
+	List<OrderItem> findAllOrderItemsByOrderId(Integer orderId);
 }
--- JavaWebCart/src/main/java/cart/service/OrderService.java
+++ JavaWebCart/src/main/java/cart/service/OrderService.java
@@ -6,8 +6,8 @@ import cart.model.dto.OrderDTO;
 import cart.model.dto.ProductDTO;
 
 public interface OrderService {
-	//建立訂單(使用者id, 購物車資料)
+	// 建立訂單(使用者 id, 購物車資料)
 	void addOrder(Integer userId, List<ProductDTO> cart);
-	//查詢使用者的訂單紀錄
-	List<OrderDTO> finadAllOrdersByUserId(Integer userId);
+	// 查詢使用者的訂單紀錄
+	List<OrderDTO> findAllOrdersByUserId(Integer userId);
 }
